cf84278772500e820549049c778c380acf14ee24
Rachana Shrestha <rachanashrestha@Rachanas-MacBook-Air.local>
2021-02-20 15:14:26 -0500

    Added one click listenere

--- app/src/main/java/com/example/codepath/MainActivity.java
+++ app/src/main/java/com/example/codepath/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.codepath;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView questionTextView= findViewById(R.id.Question1);
+        TextView answerTextView= findViewById(R.id.Answer1);
+        questionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                questionTextView.setVisibility(View.INVISIBLE);
+                answerTextView.setVisibility(View.VISIBLE);
+            }
+
+        });
+        answerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerTextView.setVisibility(View.INVISIBLE);
+                questionTextView.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+
     }
+
+
 }
