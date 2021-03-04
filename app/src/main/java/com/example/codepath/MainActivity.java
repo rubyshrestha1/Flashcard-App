c8243814aabdf86ea2a204530a0e33de65bdc2d6
Rachana Shrestha <rachanashrestha@Rachanas-MacBook-Air.local>
2021-03-04 00:23:19 -0500

    Added three options and change the background color as per the option's nature: red if wrong and green if true

--- app/src/main/java/com/example/codepath/MainActivity.java
+++ app/src/main/java/com/example/codepath/MainActivity.java
@@ -13,8 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView questionTextView= findViewById(R.id.Question1);
-        TextView answerTextView= findViewById(R.id.Answer1);
+        TextView questionTextView = findViewById(R.id.Question1);
+        TextView answerTextView = findViewById(R.id.Answer1);
+        TextView option1TextView = findViewById(R.id.Option1);
+        TextView option2TextView = findViewById(R.id.Option2);
+        TextView option3TextView = findViewById(R.id.Option3);
+
+
         questionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,7 +37,54 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.parent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.Option1).setBackgroundColor(getResources().getColor(R.color.optionBackground));
+                findViewById(R.id.Option2).setBackgroundColor(getResources().getColor(R.color.optionBackground));
+                findViewById(R.id.Option3).setBackgroundColor(getResources().getColor(R.color.optionBackground));
+            }
+        });
 
+        String answer= "Third Monday of January";
+        option1TextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (option1TextView.getText().equals(answer)){
+                    option1TextView.setBackgroundColor(getResources().getColor(R.color.green));
+                }
+                else{
+                    option1TextView.setBackgroundColor(getResources().getColor(R.color.red));
+                }
+
+            }
+        });
+
+        option2TextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (option2TextView.getText().equals(answer)){
+                    option2TextView.setBackgroundColor(getResources().getColor(R.color.green));
+                }
+                else{
+                    option2TextView.setBackgroundColor(getResources().getColor(R.color.red));
+                }
+
+            }
+        });
+
+        option3TextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (option3TextView.getText().equals(answer)){
+                    option3TextView.setBackgroundColor(getResources().getColor(R.color.green));
+                }
+                else{
+                    option3TextView.setBackgroundColor(getResources().getColor(R.color.red));
+                }
+
+            }
+        });
     }
 
 
